8c632ee16e17e007fe331147c12861422e02bfdb
pepekp <pperovmailbox@gmail.com>
2025-07-25 19:11:09 +0300

    some changes

--- c_wsp_confgen.py
+++ c_wsp_confgen.py
@@ -6,7 +6,7 @@ User input require:
 CE ASN
 CPE WAN interface (Cisco)
 SLID - id on format 312313-31313123 (only digits)
-Loopback IP
+Loopback IP - could be omitted or replaced with oter interface
 PE IF - Juniper interface
 
 Generate junos L3VPN specific configuration with BGP PE-CE routing.
@@ -19,49 +19,56 @@ import readline
 from jinja2 import Environment, FileSystemLoader
 
 print("#################################################################")
-print("####       WSP PE - CE Configuration generator               ####")
+print("#################################################################")
+print("####     Juniper PE - Cisco CE Configuration generator       ####")
+print("#################################################################")
 print("#################################################################")
 print("")
-#This line uses the current directory
-file_loader = FileSystemLoader('.')
 
 #IPCALC
 
-#net = input("WAN: ")
 
-if __name__=="__main__":
-    ipi = ipaddress.ip_interface(sys.argv[1])
-#    net = ipaddress.ip_network(sys.argv[1])
-    
+def cfg_gen():
+    # Use current directory
+    file_loader = FileSystemLoader('.')
+
+
+
+    #User Input start here.
+    ipi = input("P2P NETWORK: ")
+    peifc = input("PE Interface: ")
+    vrf_name = input("PE VRF name: ")
+    ceasn = input("CE ASN: ")
+
+    ce_vrf_name = input("CE VRF name")
+    cpewan = input("CPE WAN Interface: ")
+    slidid = input("POP SLID: ")
+    LoIP = input("Loopback: ")
+
+
+
+    ipi = ipaddress.ip_interface(ipi)
+
     print("Address", ipi.ip)
     print("Mask", ipi.netmask)
-    print("Network", str(ipi.network).split('/')[0])  
+    print("Network", str(ipi.network).split('/')[0])
     print("Broadcast", ipi.network.broadcast_address)
+    wanip = (ipi.ip) + 1
+    GW = (ipi.ip) + 2
 
-#User Input start here.
+    # slid split
+    hslid = slidid.split("-")[1]
+    #print(hslid)
 
-casn = input("CE ASN: ")
-vrf_name = input("PE VRF name: ")
-ce_vrf_name = input("CE VRF name")
-cpewan = input("CPE WAN IF: ")
-slidid = input("POP SLID: ")
-LoIP = input("Loopback: ")
-peifc = input("PE IF: ")
-WANIP = (ipi.ip) + 1
-GW = (ipi.ip) + 2
+    # Load jinja environment
+    env = Environment(loader=file_loader)
+    template = env.get_template('template.j2')
+    # Add the variables
+    output = template.render(local_asn=ceasn, vrf_name=vrf_name, ce_vrf_name=ce_vrf_name, bgp_nbr=wanip, wanif=cpewan, ce_wan=GW, lo_addr=LoIP, slid=slidid, jslid=hslid, peif=peifc)
+    # Print template
+    print(output)
+    print("")
+    print("")
 
-
-# slid split
-hslid = slidid.split("-")[1]
-#print(hslid)
-
-# Load the environment
-env = Environment(loader=file_loader)
-template = env.get_template('j_wsp.j2')
-#Add the variables
-output = template.render(local_asn=casn, vrf_name=vrf_name, ce_vrf_name=ce_vrf_name, bgp_nbr=WANIP, wanif=cpewan, ce_wan=GW, lo_addr=LoIP, slid=slidid, jslid=hslid, peif=peifc)
-#Print template
-print(output)
-print("")
-print("")
-
+if __name__=="__main__":
+    cfg_gen()
