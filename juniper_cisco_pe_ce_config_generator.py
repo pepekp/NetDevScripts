def03ff4fef615ca08b9cea103777605eceb1714
pepekp <pperovmailbox@gmail.com>
2025-07-25 20:55:47 +0300

    code changes

--- juniper_cisco_pe_ce_config_generator.py
+++ juniper_cisco_pe_ce_config_generator.py
@@ -6,7 +6,6 @@ User input require:
 CE ASN
 CPE WAN interface (Cisco)
 SLID - id on format 312313-31313123 (only digits)
-Loopback IP - could be omitted or replaced with oter interface
 PE IF - Juniper interface
 
 Generate junos L3VPN specific configuration with BGP PE-CE routing.
@@ -39,32 +38,26 @@ def cfg_gen():
     peifc = input("PE Interface: ")
     vrf_name = input("PE VRF name: ")
     ceasn = input("CE ASN: ")
-
-    ce_vrf_name = input("CE VRF name")
+    ce_vrf_name = input("CE VRF name: ")
     cpewan = input("CPE WAN Interface: ")
-    slidid = input("POP SLID: ")
-    LoIP = input("Loopback: ")
-
-
+    desc = input("Description: ")
 
     ipi = ipaddress.ip_interface(ipi)
 
     print("Address", ipi.ip)
     print("Mask", ipi.netmask)
     print("Network", str(ipi.network).split('/')[0])
-    print("Broadcast", ipi.network.broadcast_address)
+    network_addr = ipi.network
     wanip = (ipi.ip) + 1
-    GW = (ipi.ip) + 2
-
-    # slid split
-    hslid = slidid.split("-")[1]
-    #print(hslid)
+    gw = (ipi.ip) + 2
+    netmask = ipi.netmask
 
     # Load jinja environment
     env = Environment(loader=file_loader)
     template = env.get_template('template.j2')
-    # Add the variables
-    output = template.render(local_asn=ceasn, vrf_name=vrf_name, ce_vrf_name=ce_vrf_name, bgp_nbr=wanip, wanif=cpewan, ce_wan=GW, lo_addr=LoIP, slid=slidid, jslid=hslid, peif=peifc)
+    # Render variables
+    output = template.render(local_asn=ceasn, vrf_name=vrf_name, ce_vrf_name=ce_vrf_name, bgp_nbr=wanip, wanif=cpewan,
+                             ce_wan=gw, desc=desc, peif=peifc, netmask=netmask, network_addr=network_addr)
     # Print template
     print(output)
     print("")
